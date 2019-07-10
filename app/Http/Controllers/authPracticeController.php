9cd7c060262932ab4df99d1c4016382b61d626fe
mingren.chen16 <mingren.chen16@gmail.com>
2019-07-10 16:07:27 +0800

    add authPracticeController.php for api_token test

--- app/Http/Controllers/authPracticeController.php
+++ app/Http/Controllers/authPracticeController.php
@@ -1,11 +1,35 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class authPracticeController extends Controller
 {
+    function create(Request $request) {
+        return User::create([
+            'user_name' => $request->user_name,
+            'api_token' => Str::random(60)
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $token = Str::random(60);
+//        dd(User::where('user_name', $request->user_name)->first());
+        $request = User::where('user_name', $request->user_name)->first();
+//        $request->user()->forceFill([
+//            'api_token' => hash('sha256', $token),
+//        ])->save();
+
+        $request->forceFill([
+            'api_token' => hash('sha256', $token),
+        ])->save();
+        return ['token' => $token];
+    }
+
+
     function featureA() {
 
         $user_name = session('user_name');
